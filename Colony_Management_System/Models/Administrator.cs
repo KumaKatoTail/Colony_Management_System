dd76fdca73dbae7adbb3aa8518d6face31252dea
Piotr <p.szymanski@groszek.pl>
2025-01-22 10:45:00 +0100

    Początek modelowania

---
+++ Colony_Management_System/Models/Administrator.cs
@@ -0,0 +1,15 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Colony_Management_System.Models
+{
+    public class Administrator
+    {
+        public int Id { get; set; }
+        public int KontoId { get; set; }
+        public int FirmaId { get; set; }
+
+        // Navigation properties
+        public Konto Konto { get; set; }
+        public Firma Firma { get; set; }
+    }
+}
